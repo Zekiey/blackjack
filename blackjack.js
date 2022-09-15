9a80f3b78e6269fa2afc689c5a6addc4dc650de6
divine <divinerichard007@gmail.com>
2022-09-14 18:31:17 -0700

    divines part

--- blackjack.js
+++ blackjack.js
@@ -1,22 +1,37 @@
- let firstCard=6
- let secondCard=9
- let sum=firstCard+secondCard
- let hasBlackJack=false
- let isALive=true
- let message=""
+ let firstCard = 10
+ let secondCard = 4
+ let sum = firstCard + secondCard
+ let hasBlackJack = false
+ let isALive = true
+ let message = ""
+ let messageEl = document.getElementById("message-el")
+ let sumEl = document.getElementById("sum-el")
+ let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
-   if(sum<=20){
+   cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+   sumEl.textContent = "Sum: " + sum
+   if(sum <= 20){
       message='Do you want to draw a new card'
    }
-   else if(sum===21){
-      message="Wohoo! You've got blackjack"
-      hasBlackJack=true
+   else if(sum === 21){
+      message = "Wohoo! You've got blackjack"
+      hasBlackJack = true
    }
    else{
-      message="You're out of the game" 
-      isALive=false
+      message = "You're out of the game" 
+      isALive = false
    }
+   messageEl.textContent = message 
+}
+
+function newCard() {
+   console.log("Drawing a new card from the deck!")
+   let card = 7
+
+   sum += card
+
+   startGame();
 }
 
  
